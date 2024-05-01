e95ee2d795bcf796f36fedfdc8b32a295ddefb61
NikkiGetsee <Nicola.getsee@gmail.com>
2024-05-01 12:28:02 +0000

    Retrieve latest stock numbers from spreadsheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -66,6 +67,31 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data) # appends a row to the sales sheet
     print("Sales worksheet updated sucessfully \n")
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def calculate_surplus_data(sales_data):
+    '''
+    Compare sales with stock and calculate the surplus for each item type.
+
+    The surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out
+
+    '''
+    print("Calculating surplus data ... \n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    # the above states we want to get all data from the stock sheet
+    stock_row = stock[-1] #use a slice to access last row in stock list
+    print(stock_row)
+
+def main():
+
+    '''
+    Run all program functions (in python functions should be wrapped in a main function)'''
+
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+print("Welcome to Love Sandwiches Data Automation") #this is where our instructions will be for our game
+
+main()
